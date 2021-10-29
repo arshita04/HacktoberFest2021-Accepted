15fd67efd1d02809c7d5de31074f7ad586eed1a2
Arshita Narayan <49998029+arshita04@users.noreply.github.com>
2021-10-30 01:32:47 +0530

    Removed Syntax Error in Code
    Class Name in java doesn't contain brackets

--- java/IsPrime.java
+++ java/IsPrime.java
@@ -1,8 +1,8 @@
-public class isPrime(){
+public class isPrime{
 public static boolean isPrime(int number){
   int cont = 2;
   boolean prime=true;
-  while ((prime) && (cont!=number)){
+  while (prime && cont!=number){
     if (number % cont == 0)
       prime = false;
     cont++;
